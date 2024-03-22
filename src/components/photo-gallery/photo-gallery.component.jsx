34050edeb291574b5db475f6acecc29a7df1baf9
chris <chrifrat1@gmail.com>
2024-03-22 13:12:21 +0000

    photo gallery update

--- src/components/photo-gallery/photo-gallery.component.jsx
+++ src/components/photo-gallery/photo-gallery.component.jsx
@@ -5,25 +5,15 @@ import { useEffect, useState } from "react";
 // eslint-disable-next-line react/prop-types
 
 const PhotoGallery = ({ image }) => {
-  const [expandedIndex, setExpandedIndex] = useState(null);
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
-  const handleImageHover = (index) => {
-    setExpandedIndex(index); // Expand the hovered image
-  };
-
-  const handleImageLeave = () => {
-    setExpandedIndex(null); // Collapse the image when mouse leaves
+  const toggleFullScreen = () => {
+    setIsFullScreen(!isFullScreen);
   };
 
   return (
-    <div className="photo-gallery">
-      <img
-        src={image}
-        alt={`Image ${image}`}
-        className={expandedIndex !== null ? "expanded" : ""}
-        onMouseEnter={() => handleImageHover(0)} // Set index to 0 for single image
-        onMouseLeave={handleImageLeave}
-      />
+    <div className={`photo-gallery ${isFullScreen ? "full-screen" : ""}`}>
+      <img src={image} alt={`Image ${image}`} onClick={toggleFullScreen} />
     </div>
   );
 };
